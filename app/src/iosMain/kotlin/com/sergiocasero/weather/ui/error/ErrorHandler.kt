5442caaef7d01aae3b1be1766fdcf12481eaadba
Sergio Casero <sergio.casero23@gmail.com>
2019-12-04 19:16:30 +0100

    Added gradle wrapper

--- app/src/iosMain/kotlin/com/sergiocasero/weather/ui/error/ErrorHandler.kt
+++ app/src/iosMain/kotlin/com/sergiocasero/weather/ui/error/ErrorHandler.kt
@@ -1,4 +1,9 @@
 package com.sergiocasero.weather.ui.error
 
+import com.sergiocasero.weather.domain.Error
+
 actual class ErrorHandler {
+    actual fun convert(error: Error): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
